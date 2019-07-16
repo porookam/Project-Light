4789a6d63a0d788098e74ce3bf9a7393b56de346
Dmitry Kravets <porookam@gmail.com>
2019-07-16 18:48:22 +0300

    Initial Commit

---
+++ Project Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Light
+//
+//  Created by Dmitry Kravets on 16/07/2019.
+//  Copyright © 2019 Dmitry Kravets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
